d155971a626eec41b74588ea9c62be54ff3a277b
Robert Warren <grindnmosh@gmail.com>
2014-06-03 23:03:39 -0700

    Forgot to add new files

---
+++ Week1/Java2_Warren/TwitWorld/src/main/java/com/grinddesign/java2_warren/classgroup/TServ.java
@@ -0,0 +1,16 @@
+package com.grinddesign.java2_warren.classgroup;
+
+import android.app.Service;
+import android.content.Intent;
+import android.os.IBinder;
+
+public class TServ extends Service {
+    public TServ() {
+    }
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        // TODO: Return the communication channel to the service.
+        throw new UnsupportedOperationException("Not yet implemented");
+    }
+}
