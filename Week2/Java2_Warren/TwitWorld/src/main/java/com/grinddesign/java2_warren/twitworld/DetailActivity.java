6896a4be70b1047c5cf6de78f73a281b83cd3ece
Robert Warren <grindnmosh@gmail.com>
2014-06-07 20:39:22 -0700

    created xml file for detail view created planned layout for detail page created class for detail page rebased

---
+++ Week2/Java2_Warren/TwitWorld/src/main/java/com/grinddesign/java2_warren/twitworld/DetailActivity.java
@@ -0,0 +1,16 @@
+package com.grinddesign.java2_warren.twitworld;
+
+/**
+ * Author:  Robert Warren
+ * <p/>
+ * Project:  Java2_Warren
+ * <p/>
+ * Package: com.grinddesign.java2_warren.twitworld
+ * <p/>
+ * File:    ${File_Name}
+ * <p/>
+ * Purpose: ${Comments_Here}
+ */
+public class DetailActivity {
+
+}
