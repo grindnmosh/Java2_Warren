b7316fb91a81f5e830f8d089aabc96697c3d8234
Robert Warren <grindnmosh@gmail.com>
2014-06-25 09:09:07 -0700

    restore a line of code in star Activity

--- Week4/Java2_Warren/TwitWorld/src/main/java/com/grinddesign/java2_warren/twitworld/StarActivity.java
+++ Week4/Java2_Warren/TwitWorld/src/main/java/com/grinddesign/java2_warren/twitworld/StarActivity.java
@@ -3,13 +3,8 @@ package com.grinddesign.java2_warren.twitworld;
 import android.app.Activity;
 import android.content.Context;
 import android.os.Bundle;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-
-import com.grinddesign.java2_warren.classgroup.FilingCabinet;
 
 import java.util.ArrayList;
 
@@ -28,18 +23,17 @@ public class StarActivity extends Activity {
     public static ArrayList<String> starGrabber;
     public static ArrayAdapter<String> mainListAdapter;
     Context context;
-    static FilingCabinet s_File;
+    //static FilingCabinet s_File;
 
 
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
 
         setContentView(R.layout.activity_starry);
 
-        View view = inflater.inflate(R.layout.activity_starry, container, false);
 
         starGrabber = new ArrayList<String>();
 
-        final ListView lv = (ListView) view.findViewById(R.id.tList);
+        final ListView lv = (ListView) findViewById(R.id.tList);
 
         mainListAdapter = new custAdapter(context, R.layout.item_cell, starGrabber);
 
@@ -52,7 +46,5 @@ public class StarActivity extends Activity {
         lv.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
 
 
-
-        return view;
     }
 }
